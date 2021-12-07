bb79346f56a4f9d3f07b2885e937f3b6b36604bd
AlexseiLarin <zlodei328.udodov@mail.ru>
2021-12-07 21:55:12 +0300

    gotovo vrode

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,11 +2,55 @@ from django.shortcuts import render
 
 # Create your views here.
 
+links_menu = [
+    {'href': 'products_all', 'name': 'все'},
+    {'href': 'products_home', 'name': 'дом'},
+    {'href': 'products_office', 'name': 'офис'},
+    {'href': 'products_modern', 'name': 'модерн'},
+    {'href': 'products_classic', 'name': 'классика'},
+
+]
+
+menu_main = [
+    {'menu_session': 'main', 'name': 'Главная'},
+    {'menu_session': 'products', 'name': 'Продукты'},
+    {'menu_session': 'contact', 'name': 'Контакты'},
+
+]
+
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {
+        'title': 'Главная',
+        'menu_main': menu_main,
+    }
+    return render(request, 'mainapp/index.html', content)
 
 def products(request):
-    return  render(request, 'mainapp/products.html')
+    content = {
+        'title': 'Товары',
+        'links_menu': links_menu,
+        'menu_main': menu_main,
+    }
+    return  render(request, 'mainapp/products.html', content)
 
 def contact(request):
-    return  render(request, 'mainapp/contact.html')
+    content = {
+        'title': 'Контакты',
+        'menu_main': menu_main,
+    }
+    return  render(request, 'mainapp/contact.html', content)
+
+def context(request):
+    content = {
+        "title": "подобие ikea",
+        "header": "Доброго времени суток",
+        "username": "Главный генеральный директор: Ларина Е.В.",
+        "products": [
+            {"name": "Стулья", "price": 5600},
+            {"name": "Пуфы", "price": 3200},
+            {"name": "Кровати", "price": 12900},
+            {"name": "Диваны", "price": 82700},
+        ]
+    }
+
+    return render(request, "mainapp/context.html", content)
